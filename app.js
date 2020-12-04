ade3046d64c22f85e79d4006dcbc13b7ac6b8c28
ya_sergey@yahoo.com <S32069257b>
2020-12-04 12:39:57 +0300

    ssss

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const prospectRouter = require('./routes/prospect');
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/api/prospect', prospectRouter);
 
--- routes/prospect.js
+++ routes/prospect.js
@@ -1,6 +1,8 @@
-var express = require('express');
+const express = require('express');
 
-var router = express.Router();
+const router = express.Router();
+const multer = require('multer');
+const upload = multer();
 
 const sql = require('mssql');
 const db = require('../config/db');
@@ -22,12 +24,44 @@ router.get('/activities/:id', (req, res, next) => {
       }); // request.query
   }); // sql.conn
 });
-
-router.post('/save_client_status', (req, res, next) => {
+router.get('/activators', (req, res, next) => {
   sql.connect(db, (err) => {
     if (err) console.log(err);
     const request = new sql.Request();
-    console.log(req.query, req.body);
+    request
+      .query('select\n'
+          + '\t\ttop 10\n'
+          + '\t\tcnbc.*\n'
+          + '\t\t,cnbc.i_acc_opn as fdateopen\n'
+          + '\t\t,cca.portal_counter\n'
+          + '\t\t,cca.id as clientId\n'
+          + '\t\t,ccas.status\n'
+          + '\t\t,ccas.ts_created\n'
+          + '\t\t,ccas.comment\n'
+          + '\t\t,pu.[ФИО] as fio\n'
+          + '\t\t,iif(ccas.status > 0, \'Да\', \'Нет\') as worked\n'
+          + '\t\t,iif(ccas.status = 2, \'Да\', \'Нет\') as presentation\n'
+          + '\t\tfrom\n'
+          + '\t\tcorp_new_base_clients cnbc\n'
+          + '\t\tleft join corp_client_activator cca on cca.inn = cnbc.uniquetin\n'
+          + '\t\tleft join corp_client_activator_status ccas on ccas.client_id = cca.id and ccas.id = (select max(id) from corp_client_activator_status where client_id = cca.id)\n'
+          + '\t\tleft join pro_users pu on pu.counter = cca.portal_counter\n'
+          + '\t\twhere cnbc.subsegment not in (\'Tier1.1\',\'Tier1.2\') and cnbc.region not in (\'HeadOffice\')', (error, result) => {
+        if (error) {
+          console.log(error);
+          res.send(error);
+        }
+        // var rowsCount = result.rowsAffected;
+        sql.close();
+        res.send(result.recordset);
+      }); // request.query
+  }); // sql.conn
+});
+
+router.post('/save_client_status', upload.none(), (req, res, next) => {
+  sql.connect(db, (err) => {
+    if (err) console.log(err);
+    const request = new sql.Request();
     request
       .input('id', sql.Int, req.query.id)
       .input('portalCounter', sql.Int, req.query.portalCounter)
